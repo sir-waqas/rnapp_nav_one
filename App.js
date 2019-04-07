fa291655ccf87c00b7e617355a16401ad94b1a7a
Waqas Azam <waqas.mabsapps@gmail.com>
2019-04-07 17:43:10 +0500

    Basic Navigation

---
+++ App.js
@@ -0,0 +1,63 @@
+
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+
+//Navigation Imports
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+//Details Screen View Settings
+class DetailsScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details')}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+
+      </View>
+    );
+  }
+}
+//Home Screen View Settings
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+
+      </View>
+
+    );
+  }
+}
+
+//Creation of the Stack Navigator
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen,
+  Details: DetailsScreen
+},
+  {
+    initialRouteName: "Home"
+  });
+
+//Exporting while creating the App Container
+// export default createAppContainer(AppNavigator);
+const AppContainer = createAppContainer(AppNavigator);
+export default class App extends Component {
+  render() {
+    return <AppContainer />
+  }
+}
